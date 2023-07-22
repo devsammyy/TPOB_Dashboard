4c5f1d7004e210f0ec616ef13986a017de159b35
devsammyy <yinkaadebayo31@gmail.com>
2023-07-22 10:08:24 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Header from "./components/Header"
+import './styles.css'
+
+function App() {
+  
+  return (
+    <>
+      <Header/>
+      <h1>Holla</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="top_bar">
+        <nav className="top_bar-nav">
+          <ul className="top_bar-nav_container">
+            <li className="nav_item">
+              <a href="#" className="top_bar-nav_link">Customer login</a>
+            </li>
+            <p> | </p>
+            <li className="nav_item">
+              <a href="#" className="top_bar-nav_link">TPOB user </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      
+      
+     <div className="nav_bar">
+        <nav className="top_bar-nav">
+          <ul className="top_bar-nav_container">
+            <li className="nav_item">
+              <a href="#" className="top_bar-nav_link">Customer login</a>
+            </li>
+            <p> | </p>
+            <li className="nav_item">
+              <a href="#" className="top_bar-nav_link">TPOB user </a>
+            </li>
+          </ul>
+        </nav>
+      </div> 
+    </header>
+    
+  )
+}
+
+export default Header
